cb56917ff80b68e08ff94ee09d2beafde253b641
Sachielsc <sachielsc@gmail.com>
2017-08-20 10:36:15 +1200

    ready to start

--- src/components/search_bar.js
+++ src/components/search_bar.js
@@ -1,7 +1,18 @@
-import React from 'react';
+import React, { Component } from 'react';
 
-const SearchBar = () => {
-	return <input />;
-};
+// const SearchBar = () => {
+//  return <input />;
+// };
+// above is a functional component
+
+class SearchBar extends Component {
+    render () {
+        return (
+            <input />
+        );
+    }
+}
+// above is a class-based component
+
 
 export default SearchBar;
--- src/index.js
+++ src/index.js
@@ -1,9 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+import SearchBar from './components/search_bar';
+
+
 const API_KEY = 'AIzaSyAuQCVeNfKhtRk9KlChQPT1n027DPO_5Ss';
 const App = () => {
-	return <div>Hi!</div>;
+	return (
+		<div>
+			<h2>Hi!</h2>
+			<SearchBar />
+		</div>
+	);
 }
 
 ReactDOM.render(<App />, document.querySelector('.container'));
