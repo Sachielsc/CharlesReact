a60d49f91dd23efa2298562168c12dca371f8e58
Sachielsc <sachielsc@gmail.com>
2017-08-21 13:25:26 +1200

    minor

---
+++ src/components/dev_report.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const VideoList = () => {
+
+  return (
+    <ul className="list-group">
+      d
+    </ul>
+  );
+};
+
+export default VideoList;
--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 import Youtube from 'youtube-api-search';
 import SearchBar from './components/search_bar';
 import VideoList from './components/video_list';
-
+import DevReport from './components/dev_report';
 
 const API_KEY = 'AIzaSyAuQCVeNfKhtRk9KlChQPT1nO27DPO_5Ss';
 
@@ -24,6 +24,7 @@ class App extends Component {
 				<h2>Dear Colonist, type in the key word of the videos you would like to search for:</h2>
 				<SearchBar />
 				<VideoList videos={this.state.video}/>
+				<DevReport />
 			</div>
 		);
 	}
